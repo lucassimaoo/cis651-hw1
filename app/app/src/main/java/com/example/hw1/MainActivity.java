c3e864ef59bd85a2605c9acd0d9b2fbf71b93330
Lucas Silva <lsilva@fanatics.com>
2020-04-12 13:33:48 -0300

    logcat

--- app/app/src/main/java/com/example/hw1/MainActivity.java
+++ app/app/src/main/java/com/example/hw1/MainActivity.java
@@ -3,14 +3,52 @@ package com.example.hw1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("MainActivity", "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d("MainActivity", "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("MainActivity", "onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d("MainActivity", "onRestart");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("MainActivity", "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("MainActivity", "onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("MainActivity", "onDestroy");
+        super.onDestroy();
     }
 
     public void task1(View view) {
@@ -27,4 +65,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Task3Activity.class);
         startActivity(intent);
     }
+
 }
