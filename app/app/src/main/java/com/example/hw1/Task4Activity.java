aa4a345163649ff7ea19a7f1a5cb903c365aa709
Lucas Silva <lsilva@fanatics.com>
2020-04-12 16:53:24 -0300

    task4

--- app/app/src/main/java/com/example/hw1/Task4Activity.java
+++ app/app/src/main/java/com/example/hw1/Task4Activity.java
@@ -1,23 +1,26 @@
 package com.example.hw1;
 
 import android.graphics.drawable.Drawable;
-import android.support.constraint.ConstraintLayout;
-import android.support.v7.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
+
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import com.google.android.material.snackbar.Snackbar;
 
 public class Task4Activity extends AppCompatActivity {
 
     private int imageIndex = 0;
     private MovieData data = new MovieData();
     private int multiplier = 10;
+    private GestureDetectorCompat gd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,17 +41,22 @@ public class Task4Activity extends AppCompatActivity {
                 ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(progress * multiplier, progress * multiplier);
                 img.setLayoutParams(layoutParams);
             }
-
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
             }
-
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
             }
         });
 
         final Task4Activity thiz = this;
+
+        img.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return gd.onTouchEvent(event);
+            }
+        });
 
         img.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
@@ -62,7 +70,25 @@ public class Task4Activity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(thiz, "Toast!" , Toast.LENGTH_SHORT).show();
+                Snackbar.make(v, "Snack!", Snackbar.LENGTH_SHORT).show();
+            }
+        });
 
+        gd = new GestureDetectorCompat(this, new GestureDetector.SimpleOnGestureListener(){
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                if (e1.getX() < e2.getX()) {
+                    if (imageIndex > 0) {
+                        Drawable myIcon = getResources().getDrawable( (int)data.getItem(--imageIndex).get("image") );
+                        img.setBackground(myIcon);
+                    }
+                } else {
+                    if (imageIndex < data.getSize()) {
+                        Drawable myIcon = getResources().getDrawable((int) data.getItem(++imageIndex).get("image"));
+                        img.setBackground(myIcon);
+                    }
+                }
+                return true;
             }
         });
     }
