b660467b7486941fb22e4cd28770107607c63a22
Lucas Silva <lsilva@fanatics.com>
2020-04-12 11:08:27 -0300

    task1

---
+++ app/app/src/main/java/com/example/hw1/Task1Activity.java
@@ -0,0 +1,23 @@
+package com.example.hw1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Task1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task1);
+    }
+
+    public void showMessage(View view) {
+        Button btn = (Button) view;
+        Toast toast = Toast.makeText(this, btn.getId() + " " + btn.getText(), Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
