ba8212f1cf3a90ac17f189724e888a2830da2bb8
Lucas Silva <lsilva@fanatics.com>
2020-04-29 11:17:41 -0300

    detail

--- HW2/app/src/main/java/com/example/hw2/ListFragment.java
+++ HW2/app/src/main/java/com/example/hw2/ListFragment.java
@@ -11,6 +11,8 @@ import android.widget.ListView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,14 +20,14 @@ import java.util.Map;
 
 public class ListFragment extends Fragment {
 
+    private MovieData md = new MovieData();
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.master, container, false);
 
         ListView listView = v.findViewById(R.id.list_view);
 
-        MovieData md = new MovieData();
         List<String> names = new ArrayList<>();
         for (Map movie : md.getMoviesList()) {
             names.add(movie.get("name").toString());
@@ -37,7 +39,20 @@ public class ListFragment extends Fragment {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Map map = md.getItem(position);
+                FragmentManager sfm = getFragmentManager();
+                FragmentTransaction t = sfm.beginTransaction();
+                MovieDetailFragment frag = MovieDetailFragment.newInstance((int) map.get("image"), map.get("name").toString(), map.get("year").toString(),
+                        Float.parseFloat(map.get("rating").toString()), map.get("description").toString());
 
+                if (container.findViewById(R.id.detail_container) != null) {
+                    t.replace(R.id.detail_container, frag);
+                } else {
+                    t.replace(R.id.main_container, frag);
+                }
+
+                t.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE);
+                t.commit();
             }
         });
 
